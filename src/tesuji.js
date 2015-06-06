2e3b566a0acba37bc1d0f7c03a3e9277c50c26c9
waltheri <thebestgoplayer@gmail.com>
2015-06-06 12:09:14 +0200

    Update tesuji entry point

--- src/tesuji.js
+++ src/tesuji.js
@@ -1,10 +1,10 @@
 /* index.js */
 
-var Component = require("./src/Component");
+var Component = require("./Component");
 
 var tesuji = {
 	Component: Component,
-	Model: require("./src/Model"),
+	Model: require("./Model"),
 	/*applyModel: function(rootElement, model) {
 		// handle parameters
 		if(model == null) {
@@ -15,7 +15,7 @@ var tesuji = {
 		var component = new Component.FixedComponent(rootElement);
 		component.setModel(model);
 	},*/
-	Type: require("./src/utils/Type")
+	Type: require("./utils/Type")
 }
 
 if(module && module.exports) {
